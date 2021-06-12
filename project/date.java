e9350e2674fe550536b616d5f517b97fc73b9667
서은수 <83586230+eunsu0613@users.noreply.github.com>
2021-06-12 22:26:57 +0900

    Update date.java

--- project/date.java
+++ project/date.java
@@ -2,43 +2,52 @@ package project;
 
 public class date 
 {
+	// date Fields.
 	public String month;
 	public String day;
 	public String dayOfTheWeek; 
 	public String time;
-		
+	
+	// Get a month.
 	public String getMonth() 
 	{
 		return month;
 	}
 	
+	// Get a day.
 	public String getDay() 
 	{
 		return day;
 	}
 	
+	// Get a day of the week.
 	public String getDayOfTheWeek() 
 	{
 		return dayOfTheWeek;
 	}
 	
+	// Get a time.
 	public String getTime() 
 	{
 		return time;
 	}
 	
+	// Set a month.
 	public void setMonth(String month) {
 		this.month = month;
 	}
 	
+	// Set a day.
 	public void setDay(String day) {
 		this.day = day;
 	}
 	
+	// Set a time.
 	public void setTime(String time) {
 		this.time = time;
 	}
 	
+	// Set a day of the week.
 	public void setDayOfTheWeek(String dayOfTheWeek) 
 	{
 		this.dayOfTheWeek = dayOfTheWeek;
