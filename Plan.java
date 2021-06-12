534167fec3a622af2fdaa344a54c350140a7116f
서은수 <83586230+eunsu0613@users.noreply.github.com>
2021-06-12 22:13:46 +0900

    Update Plan.java

--- Plan.java
+++ Plan.java
@@ -1,75 +1,84 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-/**
- * Implement Plan class.
- * @author Seo Eun Su.
- * last modified 2021-05-25.
- */
-public class Plan extends DateInfo {
-	private String content;
-	
-	// Declare constructors.
-	public Plan() {
-		super();
-		this.content = "No plan yet.";
-	}
-	public Plan(String day) {
-		super(day);
-		this.content = "No plan yet.";
-	}
-	public Plan(String day, String content) {
-		this(day);
-		this.content = content;
-	}
-	
-	// Add new plan.
-	public void addPlan(String day, String content) {
-		setDay(day);
-		this.content = content;
-	}
-	
-	// Display all contents of the plan.
-	public void showInfo() {
-		System.out.println(getDay() + " : " + content);
-	}
-	
-	// Calculate how much time is left until the appointed time. (appointed time - time now)
-	private String calRemainingTime() {
-		SimpleDateFormat inputDayFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
-		try {
-			Date appointmentDate = inputDayFormat.parse(getDay());
-			long calDate = appointmentDate.getTime() - getCurrentTime().getTime();
-			
-			if (calDate < 0)
-				throw new Exception("Exception: It's already past time.");
+package project;
 
-			long remainingDays = calDate / (24 * 60 * 60 * 1000);
-			calDate -= remainingDays * (24 * 60 * 60 * 1000);
-			long remainingHours = calDate / (60 * 60 * 1000);
-			calDate -= remainingHours * (60 * 60 * 1000);
-			long remainingMinutes = calDate / (60 * 1000);
-			calDate -= remainingMinutes * (60 * 1000);
-			long remainingSeconds = calDate / 1000;
-			
-			return Long.toString(remainingDays) + " days " + Long.toString(remainingHours) + ":" + Long.toString(remainingMinutes) + ":" + Long.toString(remainingSeconds);
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-			System.exit(0);
-		}
-		return null;
-	}
-	// Get a remaining time that how much time is left until the appointment.
-	public String getRemainingTime() {
-		return calRemainingTime();	// It is implemented so that calRemainingTime() can be called only when getRemainingTime is called.
-	}
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Plan extends DateInfo
+{
+	// fields.
+	public String content;
+	public String remaingDay;
 	
+	// Define methods.
 	// Get a content of the plan.
-	public String getContent() {
+	public String getcontent() 
+	{
 		return content;
 	}
+	// Get a remaining time that how much time is left until the appointment.
+	public String getRemainTime() 
+	{
+		return remaingDay;
+	}
+	
 	// Set a content of the plan.
-	public void setContent(String content) {
+	public void setContent(String content) 
+	{
 		this.content = content;
 	}
+	
+	// Set a remaining time.
+	public void setRemaingTime(String remaingTime)
+	{
+		this.remaingDay = remaingTime;
+	}
+	
+	// Get a date information.
+	public String getInfo() 
+	{
+		return  super.getMonth() +"월 "+ super.getDay() +"일 " + super.getTime() + "시 ";
+	}
+	
+	
+	// 금일날짜와 약속일자 사이에 남은 날짜(d-day)를 리턴
+	public String getRemaingTime()
+	{
+		Calendar getToday = Calendar.getInstance();
+		
+		getToday.setTime(new Date()); // 금일 날짜
+		getToday.set(Calendar.HOUR,-12); // 시간 분 초 초기화
+		getToday.set(Calendar.MINUTE,0);
+		getToday.set(Calendar.SECOND,0);
+		
+		String s_date = "2021" + super.month + super.day;
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yyyyMMdd").parse(s_date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		Calendar cmpDate = Calendar.getInstance();
+		cmpDate.set(Calendar.HOUR,0); // 시간 분 초 초기화
+		cmpDate.set(Calendar.MINUTE,0);
+		cmpDate.set(Calendar.SECOND,0);
+		cmpDate.setTime(date); // 약속일자
+		
+		long Sec = (cmpDate.getTimeInMillis() - getToday.getTimeInMillis() ) / 1000 +1; // 초 단위로 계산		
+		long Days = Sec / (24*60*60); // 일 단위로 계산
+		
+		this.setRemaingTime(String.valueOf(Days)); // string으로 변환 후 저장
+		
+		// 약속일자가 금일일자 보다 빠를 시 
+		if(Days < 0)
+		{
+			this.remaingDay = "NULL";
+		}
+		
+		return remaingDay;
+	}
+	
 }
