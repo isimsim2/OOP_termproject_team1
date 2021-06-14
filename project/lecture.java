df1513a2235f84e58aad199e366251dc93a603c1
isimsim2 <55643858+isimsim2@users.noreply.github.com>
2021-06-14 19:54:13 +0900

    Update lecture.java
    remark

--- project/lecture.java
+++ project/lecture.java
@@ -1,96 +1,27 @@
-package project;
 
-public class lecture extends date
-{
-	public String subject;
-	public String course;
-	public String period;
-	public String professorName;
-	public String lectureRoom;
-	public String email;
-	public String webexLink;
+public class Lecture {
+	String subject;
+	String period;
+	String professorName;
+	String lectureRoom;
+	String email;
+	String phoneNumber;
+	String webexLink;
 	
-	public String getSubject() 
-	{
-		return subject;
-	}
-	
-	public String getcourse() 
-	{
-		return course;
-	}
-	
-	public String getPeriod() 
-	{
-		return period;
-	}
-	
-	public String getProfessorName() 
-	{
-		return professorName;
-	}
-	
-	public String getLectureRoom() 
-	{
-		return lectureRoom;
-	}
-	
-	public String getEmail() 
-	{
-		return email;
-	}
-	
-	public String getLink() 
-	{
-		return webexLink;
-	}
-	
-	
-	public void setSubject(String subject) 
-	{
+	public void setInfo(String subject, String period, String professorName, String lectureRoom, String email,
+			String phoneNumber, String webexLink) {
 		this.subject = subject;
-	}
-	
-	public void setCourse(String course) 
-	{
-		this.course = course;
-	}
-	
-	public void setPeriod(String period) 
-	{
 		this.period = period;
-	}
-	
-	public void setProfessorName(String professorName) 
-	{
 		this.professorName = professorName;
-	}
-	
-	public void setLectureRoom(String lectureRoom) 
-	{
 		this.lectureRoom = lectureRoom;
-	}
-	
-	public void setEmail(String email) 
-	{
 		this.email = email;
-	}
-	
-	public void setLink(String webexLink) 
-	{
+		this.phoneNumber = phoneNumber;
 		this.webexLink = webexLink;
 	}
 	
-	
-	/*
-	 * 다른 오브젝트와 시간과 요일이 동일한지 확인
-	 * */
-	public boolean equals(Object obj) 
-	{
-		if(this.time == ((lecture)obj).time && this.dayOfTheWeek == ((lecture)obj).dayOfTheWeek) return true;
-		else return false;	
+	public String[] getInfo() {
+		String[] tmp_info = {this.subject, this.period, this. professorName, this. lectureRoom, this. email,
+				this. phoneNumber, this. webexLink};
+		return tmp_info;
 	}
-	
-	
-	
 }
