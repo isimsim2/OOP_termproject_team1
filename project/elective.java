398cec55f63620fef8ba10e8a00f4dcac242e52d
isimsim2 <55643858+isimsim2@users.noreply.github.com>
2021-06-14 19:50:46 +0900

    Update elective.java
    remark

--- project/elective.java
+++ project/elective.java
@@ -1,5 +1,19 @@
-package project;
-
-public class elective extends lecture{
-
+public class Elective extends Lecture{
+	int electiveCredit;
+	//method that number of electiveCredit is 2 or 3 
+	public void Credit(){
+		int count = 0;
+		for (int i=0; i<super.period.length(); i++) {
+			try{
+				count += Character.getNumericValue(super.period.charAt(i));
+			} catch (Exception e) {
+				
+			}
+		}
+		if (count > 2) {
+			this.electiveCredit = 3;
+		} else {
+			this.electiveCredit = 2;
+    }	
+	}
 }
