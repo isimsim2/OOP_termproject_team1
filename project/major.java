93c3c7a91e6062be53400f38482ed1f853e07ba2
isimsim2 <55643858+isimsim2@users.noreply.github.com>
2021-06-14 19:55:11 +0900

    Update major.java
    remark

--- project/major.java
+++ project/major.java
@@ -1,4 +1,19 @@
-package project;
-
-public class major extends lecture {	
+public class Major extends Lecture {
+	int majorCredit;
+	//method that number of electiveCredit is 2 or 3
+	public void Credit(){
+		int count = 0;
+		for (int i=0; i<super.period.length(); i++) {
+			try{
+				count += Character.getNumericValue(super.period.charAt(i));
+			} catch (Exception e) {
+				
+			}
+		}
+		if (count > 2) {
+			this.majorCredit = 3;
+		} else {
+			this.majorCredit = 2;
+		}
+	}
 }
